35cfe14b89b4f36d3f1919f2fbe14e7315e5f276
HosseinEsteaneh <70004126+HosseinEsteaneh@users.noreply.github.com>
2020-09-30 13:52:28 -0700

    Add files via upload

---
+++ Python-Challenge/PyBank/main.py
@@ -0,0 +1,75 @@
+# PyBank
+import os
+import csv
+
+# Variables
+total_months = 0
+net_amount = 0
+monthly_change = []
+month_count = []
+greatest_increase = 0
+greatest_increase_month = 0
+greatest_decrease = 0
+greatest_decrease_month = 0
+
+# Path 
+csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
+
+# CSV File
+with open(csvpath, newline='') as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    csv_header = next(csvreader)
+    row = next(csvreader)
+    
+    previous_row = int(row[1])
+    total_months += 1
+    net_amount += int(row[1])
+    greatest_increase = int(row[1])
+    greatest_increase_month = row[0]
+    
+    for row in csvreader:
+        
+        total_months += 1
+        net_amount += int(row[1])
+
+        revenue_change = int(row[1]) - previous_row
+        monthly_change.append(revenue_change)
+        previous_row = int(row[1])
+        month_count.append(row[0])
+        
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            greatest_increase_month = row[0]
+            
+        if int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            greatest_decrease_month = row[0]  
+        
+    average_change = sum(monthly_change)/ len(monthly_change)
+    
+    highest = max(monthly_change)
+    lowest = min(monthly_change)
+
+# Print Analysis
+print(f"Financial Analysis")
+print(f"---------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_amount}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
+print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
+
+# Text File
+output_file = os.path.join('.', 'Resources', 'budget_data_analysis.text')
+
+with open(output_file, 'w',) as txtfile:
+
+    txtfile.write(f"Financial Analysis\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Months: {total_months}\n")
+    txtfile.write(f"Total: ${net_amount}\n")
+    txtfile.write(f"Average Change: ${average_change}\n")
+    txtfile.write(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})\n")
+    txtfile.write(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})\n")
